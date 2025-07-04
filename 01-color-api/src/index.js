635d906ff78f9b65ae6d60a7e3863146d86e03a5
Muse Tesfaye <tesfaye.musee@gmail.com>
2025-07-04 11:43:19 -0400

    feat: fetch hostname info

--- 01-color-api/src/index.js
+++ 01-color-api/src/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const os = require("os");
 
 const app = express();
 const port = 80;
+const color = "red";
+const hostname = os.hostname();
 
 app.get("/", (req, res) => {
-  res.send("hello from color-api!");
+  res.send(`<h1 style="color${color};">hello from color-api!</h1><h2>hostname: ${hostname}</h2>`);
+});
+
+app.get("/api", (req, res) => {
+  console.log(req.query);
+  if (req.query.format === "json") {
+    return res.json({
+      color,
+      hostname,
+    });
+  } else {
+    return res.send(`color: ${color}\nhostname: ${hostname}`);
+  }
 });
 
 app.listen(port, () => {
